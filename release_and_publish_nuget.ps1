abb4ef8c9e2ace294e097c8143efcd48481d677c
Tran Huy <trdtranduchuy@gmail.com>
2023-12-24 17:37:45 +0700

    update

--- release_and_publish_nuget.ps1
+++ release_and_publish_nuget.ps1
@@ -206,7 +206,7 @@ function Create-NewRelease ($TagName, $ReleaseName, $ReleaseBody, $AssetPath, $A
 		tag_name         = $TagName
 		target_commitish = $BRANCH
 		name             = $ReleaseName
-		body             = "Update and fix minor bugs[#10] Hứa"
+		body             = "Update and fix minor bugs: `n[#10] Hứa"
 		draft            = $false
 		prerelease       = $false
 	} | ConvertTo-Json
